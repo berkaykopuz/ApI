9e0817fe3c5f5cfefca513f126cb33196ce42339
berkaykopuz <berkaykopuz@hotmail.com>
2025-08-18 11:30:18 +0300

    add mistralai compatibility with openai

--- src/main/java/net/kopuz/ApI/service/AIService.java
+++ src/main/java/net/kopuz/ApI/service/AIService.java
@@ -2,26 +2,33 @@ package net.kopuz.ApI.service;
 
 import net.kopuz.ApI.exception.BadQueryRequestException;
 import net.kopuz.ApI.tool.DateTool;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AIService {
-    private final ChatClient chatClient;
     private final DateTool dateTool;
+    private final OpenAiChatModel chatModel;
 
-    public AIService(ChatClient.Builder builder, DateTool dateTool) {
-        this.chatClient = builder.build();
+    public AIService(DateTool dateTool, OpenAiChatModel chatModel) {
         this.dateTool = dateTool;
+        this.chatModel = chatModel;
     }
 
-    public String chat(String prompt){
+    public String chat(String query){
 
-        if(prompt.isEmpty()){
+        if(query.isEmpty()){
             throw new BadQueryRequestException("Client entry must be fulfilled!");
         }
 
-        return chatClient.prompt(prompt).tools(dateTool).call().content();
+        ChatResponse response = chatModel.call(
+                new Prompt(
+                        query
+                ));
+
+        return response.getResult().getOutput().getText();
     }
 }
